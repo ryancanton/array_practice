3cdb436449b5977068cf8e193ed283bdb8d2ab9b
Ryan Canton <ryancanton999@gmail.com>
2022-09-14 08:47:28 -0600

    Applied methods to arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,16 @@ many_strings = ["taco" "cat" "tac" "ocat"]
 many_ints = [1 2 3 4]
 many_floats = [1.1 2.2 3.3 4.4]
 many_booleans = [true false false true]
+
+
+many_strings.pop
+#removes and returns the last element in array
+
+many_ints.shift
+#removes and returns the first element
+
+many_floats.unshift(0.0)
+#inserts argument into array at index 0
+
+many_booleans.push(true)
+#adds argument to end of array and returns new array
